48df1109a8770ef3370bc2ac34bee4d2ed2e951a
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-11-11 16:04:44 +0100

    (v2025): update TailwindCSS
    Update from 3.4.14 to 4.1.17

--- src/components/experience-card.component.tsx
+++ src/components/experience-card.component.tsx
@@ -3,8 +3,8 @@ import { ExperienceType } from "../types/experience.type";
 
 const ExperienceCard = (experience: ExperienceType) => {
     return (
-        <div className="card card-side w-[90%] self-center shadow-sm bg-secondary">
-            <p className="flex justify-center items-center ml-4 w-32 text-3xl text-center flex-shrink-0">{experience.date}</p>
+        <div className="card card-side w-[90%] self-center shadow-xs bg-secondary">
+            <p className="flex justify-center items-center ml-4 w-32 text-3xl text-center shrink-0">{experience.date}</p>
             <div className="divider divider-horizontal py-3 flex-none"></div>
             <div className="card-body pl-0 pt-2 pb-2 flex-1">
                 <div>
