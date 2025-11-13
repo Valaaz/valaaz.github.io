2861b5791e2de9bd8ae63a6d903c4b30695d0db5
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-11-13 11:38:17 +0100

    (v2025): Add mobile navigation dropdown

--- src/components/layouts/header.layout.tsx
+++ src/components/layouts/header.layout.tsx
@@ -17,16 +17,9 @@ export default function Header() {
                     </div>
                     <ul
                         tabIndex={-1}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 p-2 items-start shadow">
+                        <li><Link to="/" className="tab">Présentation</Link></li>
+                        <li><Link to="/projects" className="tab">Projets</Link></li>
                     </ul>
                 </div>
                 <h1 className="text-xl font-bold">Portfolio</h1>
