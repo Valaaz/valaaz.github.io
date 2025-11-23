3f7147ccd336938d1c9b4a5b785b46b6b42eea87
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-11-23 13:42:17 +0100

    (v2025): Fix gap

--- src/components/skill.component.tsx
+++ src/components/skill.component.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skill = ({ name, img }: { name: string, img: string }) => {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-1 md:gap-0.5">
             <img src={img} className="w-6 h-6" />
             <p className="text-center text-base/[normal]">{name}</p>
         </div>
