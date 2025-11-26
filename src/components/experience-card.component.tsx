4e36642286fd26a8190b936bc3b14ddaa79041dd
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-11-26 17:50:51 +0100

    (v2025): Add margin to divider

--- src/components/experience-card.component.tsx
+++ src/components/experience-card.component.tsx
@@ -5,7 +5,7 @@ const ExperienceCard = (experience: ExperienceType) => {
     return (
         <div className="card w-[90%] p-4 self-center card-shadow bg-primary md:card-side">
             <p className="justify-center text-center text-2xl md:flex md:items-center md:w-32 md:text-3xl md:shrink-0">{experience.date}</p>
-            <div className="divider divider-vertical m-0 flex-none md:divider-horizontal"></div>
+            <div className="divider divider-vertical m-0 mr-2.5 flex-none md:divider-horizontal"></div>
             <div className="card-body p-0 flex-1 lg:ml-2">
                 <div>
                     <h2 className="card-title">{experience.title}</h2>
