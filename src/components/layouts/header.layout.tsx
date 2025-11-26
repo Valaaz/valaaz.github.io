f9ef5436d84995b83780594a28ddd48c1c7a4b26
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-11-26 16:37:39 +0100

    (v2025): Improve header link hover

--- src/components/layouts/header.layout.tsx
+++ src/components/layouts/header.layout.tsx
@@ -52,8 +52,8 @@ export default function Header() {
                     <ul
                         tabIndex={-1}
                         className="menu dropdown-content bg-base-100 rounded-box w-max z-1 mt-3 p-2 shadow-sm">
-                        <li><Link to="/" className="tab text-base w-full justify-start">Présentation</Link></li>
-                        <li><Link to="/projects" className="tab text-base w-full justify-start">Projets</Link></li>
+                        <li><Link to="/" className="tab text-base w-full justify-start text-black">Présentation</Link></li>
+                        <li><Link to="/projects" className="tab text-base w-full justify-start text-black">Projets</Link></li>
                         <div className="divider mx-2 my-0 gap-0"></div>
                         <li>
                             <a href="/cv.pdf" target="_blank" className="text-base flex justify-between w-full">
@@ -69,9 +69,9 @@ export default function Header() {
             {/* Tab */}
             <div className="navbar-center hidden md:flex">
                 <div role="tablist" className="tabs tabs-border">
-                    <Link to="/" className="tab">Présentation</Link>
-                    <Link to="/projects" className="tab">Projets</Link>
-                    <a href="/cv.pdf" target="_blank" className="tab flex gap-0.5">
+                    <Link to="/" className="tab text-black hover:bg-primary hover:brightness-75">Présentation</Link>
+                    <Link to="/projects" className="tab text-black hover:bg-primary hover:brightness-75">Projets</Link>
+                    <a href="/cv.pdf" target="_blank" className="tab flex gap-0.5 text-black hover:bg-primary hover:brightness-75">
                         CV
                         <img src="/icons/open-line.svg" alt="Ouvrir lien" width="16px" />
                     </a>
@@ -81,7 +81,7 @@ export default function Header() {
             {/* Contact */}
             <div className="navbar-end">
                 <div className="dropdown dropdown-end">
-                    <div tabIndex={0} role="button" className="btn btn-ghost rounded-field bg-primary hover:brightness-75">Contacter
+                    <div tabIndex={0} role="button" className="btn btn-ghost rounded-field hover:bg-primary hover:brightness-75">Contacter
                         <svg className="mt-px size-2 opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
                             <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
                         </svg>
