550e683569920918ad638839cf58da6ff82a3711
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-11-27 13:57:08 +0100

    (v2025): Fix skill icon size

--- src/components/skill.component.tsx
+++ src/components/skill.component.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Skill = ({ name, img }: { name: string, img: string }) => {
     return (
-        <div className="flex flex-col items-center gap-1 md:gap-0.5">
-            <img src={img} className="w-6 h-6" />
+        <div className="flex flex-col items-center gap-1 w-max md:gap-0.5">
+            <img src={img} className="size-6" />
             <p className="text-center text-base/[normal]">{name}</p>
         </div>
     );
