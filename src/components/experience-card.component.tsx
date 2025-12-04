e588dd0abf1ce88466b919bc775299fb53114ea2
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-12-04 14:33:06 +0100

    (master): Fix chips wrap

--- src/components/experience-card.component.tsx
+++ src/components/experience-card.component.tsx
@@ -20,7 +20,7 @@ const ExperienceCard = (experience: ExperienceType) => {
                         ))}
                     </ul>
                 </div>
-                <div className="flex gap-2 justify-center md:justify-start">
+                <div className="flex flex-wrap gap-2 justify-start">
                     {experience.technologies.map((tech) => (
                         <span className="badge badge-outline py-3 chip">{tech}</span>
                     ))}
