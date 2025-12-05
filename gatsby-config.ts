0186a6ace5e1c323e0aa2f4f664c8522a6c1a54f
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-12-05 14:58:13 +0100

    (master): Improve local dev
    - Enable HMR
    - Reduce scanned files

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,12 +2,15 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://valaaz.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  flags: {
+    FAST_DEV: true,
+  },
   plugins: [
     'gatsby-plugin-postcss',
   ],
