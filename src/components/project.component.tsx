8651bb00a183128b3aad9174d8b0be4aebf048b0
Valentin AZANCOTH <val.zancoth@gmail.com>
2025-12-05 14:56:41 +0100

    (master): Add personal projects

--- src/components/project.component.tsx
+++ src/components/project.component.tsx
@@ -15,15 +15,17 @@ const Project = (projectData: ProjectType) => {
                     </span>
                 </h2>
                 <p>{projectData.description}</p>
-                <div className="flex flex-wrap gap-2 w-[90%]">
-                    {projectData.technologies.map((tech) => (
-                        <span className="badge badge-outline py-3 chip">{tech}</span>
-                    ))}
+                <div className="flex flex-row justify-between">
+                    <div className="flex flex-wrap gap-2 w-[90%]">
+                        {projectData.technologies.map((tech) => (
+                            <span className="badge badge-outline py-3 chip">{tech}</span>
+                        ))}
+                    </div>
+                    {projectData.link &&
+                        <a className="w-6 self-end-safe transition-transform hover:scale-120" href={projectData.link} target="_blank" rel="noopener noreferrer">
+                            <img src="/logos/github.svg"></img>
+                        </a>}
                 </div>
-                {projectData.link &&
-                    <a className="w-6 absolute right-4 bottom-4 transition-transform hover:scale-120" href={projectData.link} target="_blank" rel="noopener noreferrer">
-                        <img src="/logos/github.svg"></img>
-                    </a>}
             </div>
         </OnScroll>
     );
